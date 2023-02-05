e84620a7fe8b4d2e967961ee852b5b2632f42c5c
Packman5045 <packman.lp@gmail.com>
2023-02-04 23:15:25 -0500

    Initial commit;Task01

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        int price;
+        double TAX = 0.05;
+
+        price = 125;
+
+        double salesTax = price * TAX;
+
+        System.out.println("The sales tax is" + " " + salesTax);
+
+
+
+
+
+   }
+
+}
